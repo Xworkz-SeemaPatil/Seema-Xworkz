9c6fac906323f610b9fe9b45c7020f260184e033
Xworkz-SeemaPatil <seema.xworkz@gmail.com>
2020-12-09 12:18:15 +0530

    inheritance example

---
+++ calculator-inherit-proj/src/com/xworkz/cal/Calculator.java
@@ -0,0 +1,14 @@
+package com.xworkz.cal;
+
+public class Calculator {
+	
+	public void addition() {
+		System.out.println("adding two numbers");
+	}
+	
+	public void subtraction() {
+		System.out.println("subtracting two numbers");
+		
+	}
+
+}
---
+++ calculator-inherit-proj/src/com/xworkz/util/CalUtil.java
@@ -0,0 +1,16 @@
+package com.xworkz.util;
+
+import com.xworkz.cal.Calculator;
+import com.xworkz.cal.SciCal;
+
+public class CalUtil  {
+	
+	public static void main(String[] args) {
+		
+		Calculator cal=new SciCal();
+		cal.addition();
+		cal.subtraction();
+		
+	}
+
+}
