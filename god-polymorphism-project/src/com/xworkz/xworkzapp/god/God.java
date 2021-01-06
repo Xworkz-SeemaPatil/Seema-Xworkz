fb09f9b27658f4d888f0508e9e09aaa26fa1acc5
Xworkz-SeemaPatil <seema.xworkz@gmail.com>
2021-01-06 23:03:35 +0530

    polymorphism and typecasting

---
+++ god-polymorphism-project/src/com/xworkz/xworkzapp/god/God.java
@@ -0,0 +1,9 @@
+package com.xworkz.xworkzapp.god;
+
+public class God {
+	public  String address="kailasa";
+	public void bless() {
+		System.out.println("Give blessing");
+	}
+
+}
---
+++ god-polymorphism-project/src/com/xworkz/xworkzapp/god/Shiva.java
@@ -0,0 +1,10 @@
+package com.xworkz.xworkzapp.god;
+
+public class Shiva extends God{
+	
+	public  String address="murudeshwara";
+	public void bless() {
+		System.out.println("Unconditional Blessings");
+	}
+
+}
