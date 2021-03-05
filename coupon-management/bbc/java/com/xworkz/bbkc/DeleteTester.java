fffbd0acccd9205d11507228f11e9d2aad8fef18
unknown <you@example.com>
2021-03-05 15:41:44 +0530

    hibernate projects

--- coupon-management/bbc/java/com/xworkz/bbkc/DeleteTester.java
+++
@@ -1,9 +0,0 @@
-package com.xworkz.bbkc;
-
-import com.xworkz.bbkc.dao.BBCDAOImpl;
-public class DeleteTester {
-	public static void main(String[] args) {
-		new BBCDAOImpl().deleteById(2);
-		
-	}
-}
